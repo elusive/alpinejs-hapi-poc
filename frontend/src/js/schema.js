dea1599c28e93657be1526c400698d044bcc42e1
John Gilliland <john.gilliland@starz.com>
2024-01-10 23:55:09 -0700

    Initial commit for front end.

---
+++ frontend/src/js/schema.js
@@ -0,0 +1,17 @@
+ 
+ /**
+  * Function to return schema objects and support fx
+  * for the datatable output.
+  * @remarks Datetime format is YYYY-MM-DDTHH:mm:ss.sssZ
+  */
+ function schema() {
+    return {
+        "workOrders": [
+            { 'id': 1, 'orderNo': 301, 'notes': 'Repair downed line at pole XYZ on Wendall & Jackson.',  'personnel': 'R. Larsen, T. Robertson', 'created': '2024-01-08T10:45:21.000Z', 'modified': '2023-12-03 20:32:27.112Z', 'modifiedBy': 'johng' },
+            { 'id': 2, 'orderNo': 302, 'notes': 'Remove parts left by crew at pole 419A on RR 14.',  'personnel': 'E. Watson', 'created': '2023-12-05 08:23:43.343Z', 'modified': '2023-12-03 20:32:27.112Z', 'modifiedBy': 'johng' },
+            { 'id': 3, 'orderNo': 303, 'notes': 'Dig new trench along parcel Xray9Foxtrot from marker 13 to 18.',  'personnel': 'J. Johnson, P. Powell', 'created': '2023-10-12 09:44:32.556Z', 'modified': '2023-12-03 20:32:27.112Z', 'modifiedBy': 'johng' },
+            { 'id': 4, 'orderNo': 304, 'notes': 'Replace downed transformer at pole 322 on Hwy 75 mile marker 16',  'personnel': 'T. Robertson', 'created': '2023-11-22 13:57:54.923Z', 'modified': '2023-12-03 20:32:27.112Z', 'modifiedBy': 'johng' },
+        ],
+        
+    }
+ }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+// Tailwind CSS Default Configuration File
+// https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+module.exports = {
+    content: ['./src/index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+      container: {
+          center: true,
+      },
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
